86923e186a84a1563bf1184cf8d0d665c072eeba
olddeer <38377164+olddeer@users.noreply.github.com>
2025-06-20 19:15:05 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-DIFF_OUTPUT=$(diff submitted.txt expected.txt)
- 
- if [ $? -eq 0 ]; then
-     echo "good"
-     exit 0
- else
-     echo "$DIFF_OUTPUT"
-     echo "bad"
-     exit 1
- fi
+td=$(mktemp -d foooXXXXXX) && trap "rm -rf $td" EXIT
+expected="$td/expected.txt"
+submitted="$td/submitted.txt"
+
+cat expected.txt | sort -k2,2 > $expected
+cat submitted.txt | sort -k2,2 > $submitted
+
+diff $expected $submitted
+
+if [ "$?" == 0 ]; then
+    echo "good"
+    exit 0
+else
+    echo "bad"
+    exit 1
+fi
